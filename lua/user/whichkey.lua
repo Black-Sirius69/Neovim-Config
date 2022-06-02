59cade31571c57671e7bccefe304b65b2861ba99
sirius_black <vedant.matanhelia@gmail.com>
2022-06-02 12:18:20 +0530

    Added gitsigns plugin -^> a powerful git plugin

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -102,6 +102,28 @@ local mappings = {
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 		l = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Lazygit" },
 	},
+    g = {
+        name = "Git",
+        g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
+        j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+        k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+        l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+        p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+        r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+        R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+        s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+        u = {
+          "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+          "Undo Stage Hunk",
+        },
+        o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+        b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+        c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+        d = {
+          "<cmd>Gitsigns diffthis HEAD<cr>",
+          "Diff",
+        },
+  },
 }
 local opts = { prefix = "<leader>" }
 which_key.setup(setup)
