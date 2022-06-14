f2c7321cac91e7047c78707ace914eadadc138bf
sirius_black <vedant.matanhelia@gmail.com>
2022-06-14 11:27:58 +0530

    Changed theme to gruvbox and added nvim lsp signature help

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -1,8 +1,7 @@
-        -- imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
-        -- let g:copilot_no_tab_map = v:true
-local opts = {silent = true, expr = true}
+-- imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+-- let g:copilot_no_tab_map = v:true
+local opts = { silent = true, expr = true }
 local keymap = vim.api.nvim_set_keymap
 vim.g["copilot_no_tab_map"] = true
 
 keymap("i", "<C-j>", 'copilot#Accept("<CR>")', opts)
-
